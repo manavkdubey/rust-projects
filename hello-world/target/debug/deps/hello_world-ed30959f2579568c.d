8dc53f1909588f78e7b20cd99d03939ba0460c2d
manavkdubey <dmanavkumar24@gmail.com>
2023-10-18 22:45:49 +0530

    Day1:hello-world

---
+++ hello-world/target/debug/deps/hello_world-ed30959f2579568c.d
@@ -0,0 +1,5 @@
+/Users/manavkumardubey/Desktop/projects/rust/rust-projects/hello-world/target/debug/deps/hello_world-ed30959f2579568c: src/main.rs
+
+/Users/manavkumardubey/Desktop/projects/rust/rust-projects/hello-world/target/debug/deps/hello_world-ed30959f2579568c.d: src/main.rs
+
+src/main.rs:
