93fa271e1d05807476e54280da55e417044e3a2f
manavkdubey <dmanavkumar24@gmail.com>
2023-10-20 23:44:54 +0530

    Day 4: Updated commit

---
+++ hello-world/target/debug/hello-world.d
@@ -0,0 +1,1 @@
+/Users/manavkumardubey/Desktop/projects/rust/rust-projects/hello-world/target/debug/hello-world: /Users/manavkumardubey/Desktop/projects/rust/rust-projects/hello-world/src/main.rs
---
+++ random-num-guess/target/debug/deps/random_num_guess-160c2c864314bd48.d
@@ -0,0 +1,5 @@
+/Users/manavkumardubey/Desktop/projects/rust/rust-projects/random-num-guess/target/debug/deps/random_num_guess-160c2c864314bd48.rmeta: src/main.rs
+
+/Users/manavkumardubey/Desktop/projects/rust/rust-projects/random-num-guess/target/debug/deps/random_num_guess-160c2c864314bd48.d: src/main.rs
+
+src/main.rs:
